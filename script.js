9e52144b15f3e053431113a3c74651a4b51a56c3
Zaxx <98286699+zaxxdev@users.noreply.github.com>
2023-06-26 04:15:21 +0200

    var < const

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-
 function resetForm() {
     // Reset input fields
     document.getElementById("inputA").value = "0";
     document.getElementById("inputB").value = "0";
     document.getElementById("inputC").value = "0";
 }
+
 function calculateDelta() {
-    var a = parseFloat(document.getElementById("inputA").value);
-    var b = parseFloat(document.getElementById("inputB").value);
-    var c = parseFloat(document.getElementById("inputC").value);
-    var delta = b * b - 4 * a * c;
+    const a = parseFloat(document.getElementById("inputA").value);
+    const b = parseFloat(document.getElementById("inputB").value);
+    const c = parseFloat(document.getElementById("inputC").value);
+    const delta = b * b - 4 * a * c;
 
     if (delta > 0) {
-        var x1 = (-b + Math.sqrt(delta)) / (2 * a);
-        var x2 = (-b - Math.sqrt(delta)) / (2 * a);
+        const x1 = (-b + Math.sqrt(delta)) / (2 * a);
+        const x2 = (-b - Math.sqrt(delta)) / (2 * a);
         document.getElementById("result").innerHTML = "x₁ = " + x1 + ", x₂ = " + x2;
         document.getElementById("result").className = "success";
     } else if (delta === 0) {
-        var x = -b / (2 * a);
+        const x = -b / (2 * a);
         document.getElementById("result").innerHTML = "x = " + x;
         document.getElementById("result").className = "success";
     } else {
